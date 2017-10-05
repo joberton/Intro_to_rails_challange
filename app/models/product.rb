c704f8793ce003b37f65465b1bee4da6a73ebd27
joberton <joberton@academic.rrc.ca>
2017-10-05 15:21:31 +0000

    Added validation rules for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :price, presence: true
+	validates :stock_quantity, presence: true
 end
